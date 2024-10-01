667f8669ac6d2d5a7f125dbdf7f7ef985972115d
Nik-IV0601 <tuzur1983@gmail.com>
2024-10-01 21:13:04 +0300

    Add files via upload

---
+++ first/src/edu/training/jc/simple_output/FormattingWithingString.java
@@ -0,0 +1,11 @@
+package edu.training.jc.simple_output;
+
+public class FormattingWithingString 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("1) Todoist - баланс между мощностью и простотой.\n"
+				+ "2) TickTick - встроенные календари и таймеры.\n"
+				+ "3) Microsoft To Do - для опытных пользователей Microsoft.");
+	}
+}
---
+++ first/src/edu/training/jc/simple_output/FrameOutput.java
@@ -0,0 +1,22 @@
+package edu.training.jc.simple_output;
+
+public class FrameOutput 
+{
+
+	public static void main(String[] args) 
+	{
+//		вар.1
+//		System.out.print("***********\n*    *    *\n*    *    *\n*    *    *\n***********");
+		
+//		вар.2
+		System.out.println("***********");
+		System.out.println("*    *    *");
+		System.out.println("*    *    *");
+		System.out.println("*    *    *");
+		System.out.println("***********");
+		
+//		вар.3
+//		String str1 = "\n***********", str2 = "\n*    *    *";
+//		System.out.println(str1+str2+str2+str2+str1);
+	}
+}
